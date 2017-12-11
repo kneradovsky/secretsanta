b4cd951cd8f084a435c1df5dd77ff53f7534f680
Konstantin Neradovsky <konstantin.neradovskiy@open.ru>
2017-12-11 17:36:41 +0300

    santa select functions addeD

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -15,3 +15,27 @@ exports.newUser = functions.auth.user().onCreate( event => {
     var db = admin.database();
     db.ref(`/users/${user.uid}`).set({uid: user.uid, email: user.email, hasSanta : false});
 })
+
+
+exports.getSanta = admin.database().ref("/createsantas").on("child_added",(ds,prevKey) => {
+    var req = ds.val();
+    admin.database().ref("/users/").once('value',ds=> {
+        let users  = ds.val();
+        console.log(users);
+        for(i=0;i<users.length;i++) {
+            var pairIndex = (Math.random()*users.length) % users.length;
+            for(j=0;j<uses.length;j++) {
+                if(pairIndex>=users.length) pairIndex=0;
+                if(!users[pairIndex].hasSanta && users[i].uid!=users[pairIndex].uid) {
+                    users[pairIndex].hasSanta=true;
+                    users[pairIndex].santa = `${users[i].firstname} ${users[i].lastname}`
+                    admin.database().ref(`/users/${users[pairIndex].uid}`).update(users[pairIndex]);
+                    break;
+                }
+                pairIndex++;
+            }
+        }
+    })
+});
+
+
